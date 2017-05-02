fc1f0a3b7e6b4eaeca7480aca74c66c161f5efc0
Marko Jelača <jelaca.marko@gmail.com>
2017-05-02 15:22:27 +0200

    no message

---
+++ XACML_Parser/XACML_Parser/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XACML_Parser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PolicyType));
+            
+            StreamReader reader = new StreamReader("rule1.main.xml");
+            var value = serializer.Deserialize(reader);
+        }
+    }
+}
