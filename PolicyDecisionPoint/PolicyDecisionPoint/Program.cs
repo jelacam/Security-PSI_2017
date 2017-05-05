296d982ae7d920f1266abcd9b2367f7bd700ca87
Marko Jelača <jelaca.marko@gmail.com>
2017-05-05 11:38:27 +0200

    Kreiran projekat koji predstavlja PDP komponentu data flow modela. PDP ce biti servis koji razume XACML autorizacione politike.

---
+++ PolicyDecisionPoint/PolicyDecisionPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PolicyDecisionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PolicyType));
+
+            StreamReader reader = new StreamReader("rule1.main.xml");
+            var value = serializer.Deserialize(reader);
+        }
+    }
+}
