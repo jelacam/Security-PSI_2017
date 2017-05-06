36066a88decc677887fb0aaf6a77804237ccea78
Marko Jelača <jelaca.marko@gmail.com>
2017-05-06 14:03:16 +0200

    Impelemntiana dodatna provera da li se target poklapa prema specifikaciji 7.3.4 i 7.3.5 i prema 7.7 (Target Evaluation) sekcijama specifikacije.

--- PolicyDecisionPoint/PolicyDecisionPoint/XAML_Common/PolicyEvaluateManager.cs
+++ PolicyDecisionPoint/PolicyDecisionPoint/XAML_Common/PolicyEvaluateManager.cs
@@ -6,6 +6,10 @@ namespace PolicyDecisionPoint.XAML_Common
     public static class PolicyEvaluateManager
     {
 
+        public const string NO_MATCH = "noMatch";
+        public const string MATCH = "match";
+        public const string INDETERMINATE = "indeterminate";
+
 
         /// <summary>
         ///     Metoda vrsi evaluaciju po principu definisanim XACML standardom: Sekcija C.8 First-applicable
@@ -26,6 +30,10 @@ namespace PolicyDecisionPoint.XAML_Common
                 Effect = DecisionType.Deny;
             }
 
+            string AllOfValue = string.Empty;
+            string AnyOfValue = string.Empty;
+            
+            
             /// atributi pravila
             TargetType Target = rule.Target;
 
@@ -37,11 +45,20 @@ namespace PolicyDecisionPoint.XAML_Common
 
             try
             {
+
+
                 foreach (AnyOfType AnyOf in Target.AnyOf)
                 {
+                    int numberOfMatchAllOf = 0;
+                    int numberOfIndeterminateAllOf = 0;
+                    int numberOfNoMatchAllOf = 0;
+
                     AllOfType[] AllOfs = AnyOf.AllOf;
                     foreach (AllOfType AllOf in AllOfs)
                     {
+                        int numberOfFalseMatch = 0;
+                        int numberOfIndeterminateMatch = 0;
+
                         MatchType[] Matches = AllOf.Match;
                         foreach (MatchType Match in Matches)
                         {
@@ -54,33 +71,90 @@ namespace PolicyDecisionPoint.XAML_Common
 
                                 if (decision.Equals(CheckResult.False))
                                 {
-                                    return DecisionType.NotApplicable;
+                                    //return DecisionType.NotApplicable;
+                                    numberOfFalseMatch++;
                                 }
                                 else if (decision.Equals(CheckResult.Indeterminate))
                                 {
-                                    return DecisionType.Indeterminate;
+                                    //return DecisionType.Indeterminate;
+                                    numberOfIndeterminateMatch++;
                                 }
                             }
                         }
+
+                        if (numberOfFalseMatch!=0)
+                        {
+                            AllOfValue = NO_MATCH;
+                            numberOfNoMatchAllOf++;
+                        }
+                        
+                        if (numberOfFalseMatch==0 && numberOfIndeterminateMatch>0)
+                        {
+                            AllOfValue = INDETERMINATE;
+                            numberOfIndeterminateAllOf++;
+                        }
+
+                        if (numberOfFalseMatch == 0 && numberOfIndeterminateMatch == 0)
+                        {
+                            AllOfValue = MATCH;
+                            numberOfMatchAllOf++;
+                        }
+
+
+
+                    }
+
+                    if (numberOfIndeterminateAllOf > 0 && numberOfMatchAllOf == 0)
+                    {
+                        AnyOfValue = INDETERMINATE;
+                    }
+
+                    if (numberOfNoMatchAllOf > 0 && numberOfIndeterminateAllOf == 0)
+                    {
+                        AnyOfValue = NO_MATCH;
+                    }
+
+                    if (numberOfMatchAllOf > 0)
+                    {
+                        AnyOfValue = MATCH;
+                    }
+
+                    if (AnyOfValue.Equals(NO_MATCH))
+                    {
+                        return DecisionType.NotApplicable;
                     }
                 }
+
+               
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Error: {0}", e.Message);
                 return DecisionType.Indeterminate;
             }
 
-
             return Effect;
         }
 
+        /// <summary>
+        ///     Evaluacija request context-a prema Match elementu
+        /// </summary>
+        /// <param name="attributeValue">
+        ///             Vrednost sa kojom se proverava
+        /// </param>
+        /// <param name="attributeDesignator">
+        ///             Definise tip podataka koji treba da se proveravaju - pravi se bag of attributes
+        /// </param>
+        /// <param name="request"></param>
+        /// <returns></returns>
         private static CheckResult CheckIfMatchStringEqual(AttributeValueType attributeValue,
                                                  AttributeDesignatorType attributeDesignator,
                                                  RequestType request)
         {
             try
             {
+                bool exist = false;
+
                 /// atributi zahteva
                 AttributesType[] Attributes = request.Attributes;
                 foreach (AttributesType Attribute in Attributes)
@@ -88,32 +162,47 @@ namespace PolicyDecisionPoint.XAML_Common
                     if (attributeDesignator.Category.Equals(Attribute.Category))
                     {
                         //ako je kategorija jednaka proveravamo za atribute te kategorije
+                        exist = true;
                         AttributeType[] AttributesType = Attribute.Attribute;
+
                         foreach (AttributeType AttrType in AttributesType)
                         {
-                            AttributeValueType[] AttributeValues = AttrType.AttributeValue;
-                            foreach (AttributeValueType AttrValue in AttributeValues)
+                            if (attributeDesignator.AttributeId.Equals(AttrType.AttributeId))
                             {
-                                if (AttrValue.DataType.Equals(attributeValue.DataType))
+                                AttributeValueType[] AttributeValues = AttrType.AttributeValue;
+                                foreach (AttributeValueType AttrValue in AttributeValues)
                                 {
-                                    XmlNode[] node = AttrValue.Any as XmlNode[];
-                                    string value = node[0].Value;
-
-                                    XmlNode[] nodeAttr = attributeValue.Any as XmlNode[];
-                                    string valueAttr = nodeAttr[0].Value;
-
-                                    if (value.Equals(valueAttr))
+                                    if (AttrValue.DataType.Equals(attributeValue.DataType))
                                     {
-                                        return CheckResult.True;
+                                        XmlNode[] node = AttrValue.Any as XmlNode[];
+                                        string value = node[0].Value;
+
+                                        XmlNode[] nodeAttr = attributeValue.Any as XmlNode[];
+                                        string valueAttr = nodeAttr[0].Value;
+
+                                        if (value.Equals(valueAttr))
+                                        {
+                                            return CheckResult.True;
+                                        }
                                     }
                                 }
-
                             }
                         }
                     }
                 }
+
+                if (!exist)
+                {
+                    /// je bag of attributes prazan
+                    /// provera MustBePrestented atributa
+                    if(attributeDesignator.MustBePresent)
+                    {
+                        return CheckResult.Indeterminate;
+                    }
+                    
+                }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Error: {0}", e.Message);
                 return CheckResult.Indeterminate;
